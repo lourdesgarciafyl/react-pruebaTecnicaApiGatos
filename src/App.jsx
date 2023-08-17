7624bca1244bc1bc9d105e80fe6f5dfb8f524e46
LourdesGarcia <lourdesgarciafyl@gmail.com>
2023-08-17 11:10:51 -0300

    se separa en dos funciones distintas el get image y el get fact, se realiza el llamado en use effects distintos

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const CAT_IMAGE_URL_BASE = 'https://cataas.com'
-// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 
 export function App () {
   const [fact, setFact] = useState('')
-  // const [firstWord, setFirstWord] = useState('')
   const [imageURL, setImageURL] = useState('')
   useEffect(() => {
     getRandomFact()
   }, [])
+  useEffect(() => {
+    if (!fact) return
+    getImage()
+  }, [fact])
 
   const getRandomFact = async () => {
     const resp = await fetch(CAT_ENDPOINT_RANDOM_FACT)
@@ -19,7 +21,9 @@ export function App () {
     const dataFact = await data.fact
     setFact(dataFact)
     // separo la primera palabra
-    const dataFirstWord = dataFact.split(' ')[0]
+  }
+  const getImage = async () => {
+    const dataFirstWord = fact.split(' ')[0]
     // Si se pide más de una palabra
     // const threeWords = dataFact.split(" ", 3).join(" ")
     const respImage = await fetch(`https://cataas.com/cat/says/${dataFirstWord}?size=50&color=red&json=true`)
@@ -32,7 +36,7 @@ export function App () {
     <main>
       <h1>App de gatitos</h1>
       {fact && <p>{fact}</p>}
-      <img src={`${CAT_IMAGE_URL_BASE}${imageURL}`} alt={'Image extracted using the first word'} / >
+      <img src={`${CAT_IMAGE_URL_BASE}${imageURL}`} alt='Image extracted using the first word' />
     </main>
   )
 }
